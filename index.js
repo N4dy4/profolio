8d778cdfa85f01db1687208a1b2235173647bb1b
N4dy4 <150077798+N4dy4@users.noreply.github.com>
2024-09-12 17:27:56 -0400

    Trying to get everything fix

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-function openPage (evt, pageName){
+function openPage(evt, pageName) {
+    var i, pagecontent, pagelinks;
 
-var i, pagecontent, pagelinks;
+    // Hide all page content elements
+    pagecontent = document.getElementsByClassName("pagecontent");
+    for (i = 0; i < pagecontent.length; i++) {
+        pagecontent[i].style.display = "none";
+    }
 
-pagecontent = document.getElementsByClassName("pagecontent");
-for (i = 0; i < pagecontent.length; i++){
-    pagecontent[i].computedStyleMap.display = "none";
+    // Remove the 'active' class from all page links
+    pagelinks = document.getElementsByClassName("pagelinks");
+    for (i = 0; i < pagelinks.length; i++) {
+        pagelinks[i].className = pagelinks[i].className.replace(" active", "");
+    }
 
-}
-
-pagelinks = document.getElementsByClassName("pagelinks");
-for (i = 0; i < pagelinks.length; i++){
-    pagelinks[i].className = pagelinks[i].className.replace(" active", "");
-}
-
-document.getElementById(paegName).style.display = "block";
-evt.currentTarget.className += " active";
+    // Show the selected page content and add 'active' class to the clicked link
+    document.getElementById(pageName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
